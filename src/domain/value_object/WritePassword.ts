6fcd36abe641b5a6630bad1659ef1e6fdf3effa7
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-03-26 12:36:07 +0900

    強度チェックをコメントアウト

--- src/domain/value_object/WritePassword.ts
+++ src/domain/value_object/WritePassword.ts
@@ -10,8 +10,8 @@ export type Password = {
 };
 
 // Minimum 8 characters, one uppercase, one lowercase, one digit, one special character.
-const passwordRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+// const passwordRegex =
+// /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
 
 export const createPassword = (
   value: string | null
@@ -19,9 +19,10 @@ export const createPassword = (
   if (!value || value === "") {
     return err(new ValidationError("パスワードを入力してください"));
   }
-  if (!passwordRegex.test(value)) {
-    return err(new ValidationError("パスワードの強度が不足しています"));
-  }
+  // 強度チェックを行う場合はコメントアウトを外す
+  // if (!passwordRegex.test(value)) {
+  //   return err(new ValidationError("パスワードの強度が不足しています"));
+  // }
   return ok({ _type: "Password", val: value });
 };
 
