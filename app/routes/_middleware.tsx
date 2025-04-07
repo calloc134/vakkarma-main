7c8e72e194e3e84c867a0352925321ad48a13402
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-04-07 11:31:22 +0900

    pino middlewareの導入と型定義拡張

--- app/routes/_middleware.tsx
+++ app/routes/_middleware.tsx
@@ -1,7 +1,22 @@
 import { logger } from "hono/logger";
+import { pinoLogger } from "hono-pino";
 import { createRoute } from "honox/factory";
 
 import { csrf } from "../middlewares/csrfMiddleware";
 import { dbInitializeMiddleware } from "../middlewares/dbInitializeMiddleware";
 
-export default createRoute(logger(), csrf(), dbInitializeMiddleware());
+import type { PinoLogger } from "hono-pino";
+
+export default createRoute(
+  pinoLogger(),
+  logger(),
+  csrf(),
+  dbInitializeMiddleware()
+);
+
+// しょうがないので型定義を拡張
+declare module "hono" {
+  interface ContextVariableMap {
+    logger: PinoLogger;
+  }
+}
